723dd78f51d1ca2d82fef261092907b445e52c2d
Roshan-ac <roshanacharya3636@gmail.com>
2023-11-26 23:03:40 +0545

    build error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Projects", href: "/projects" },
+  { name: "Projects", href: "#" },
   { name: "Contact", href: "/contact" },
 ];
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
+    <div className="flex space-y-10 flex-col items-center justify-start w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -23,28 +23,22 @@ export default function Home() {
           ))}
         </ul>
       </nav>
+      <div className="my-6 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 ">
+         Welcome To My Landing Page
+        </h2>
+      </div>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        Resham Acharya
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
-        </h2>
-      </div>
+
     </div>
   );
 
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,41 +1,40 @@
-import Link from "next/link";
+// import Link from "next/link";
 import React from "react";
-import { allProjects } from "contentlayer/generated";
+// import { allProjects } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
-import { Card } from "../components/card";
-import { Article } from "./article";
-import { Redis } from "@upstash/redis";
-import { Eye } from "lucide-react";
+// import { Card } from "../components/card";
+// import { Article } from "./article";
+// import { Redis } from "@upstash/redis";
+// import { Eye } from "lucide-react";
 
-const redis = Redis.fromEnv();
 
-export const revalidate = 60;
+
 export default async function ProjectsPage() {
-  const views = (
-    await redis.mget<number[]>(
-      ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":")),
-    )
-  ).reduce((acc, v, i) => {
-    acc[allProjects[i].slug] = v ?? 0;
-    return acc;
-  }, {} as Record<string, number>);
+//   const views = (
+//     await redis.mget<number[]>(
+//       ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":")),
+//     )
+//   ).reduce((acc, v, i) => {
+//     acc[allProjects[i].slug] = v ?? 0;
+//     return acc;
+//   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
-  const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-  const top3 = allProjects.find((project) => project.slug === "highstorm")!;
-  const sorted = allProjects
-    .filter((p) => p.published)
-    .filter(
-      (project) =>
-        project.slug !== featured.slug &&
-        project.slug !== top2.slug &&
-        project.slug !== top3.slug,
-    )
-    .sort(
-      (a, b) =>
-        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
-    );
+//   const featured = allProjects.find((project) => project.slug === "unkey")!;
+//   const top2 = allProjects.find((project) => project.slug === "planetfall")!;
+//   const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+//   const sorted = allProjects
+//     .filter((p) => p.published)
+//     .filter(
+//       (project) =>
+//         project.slug !== featured.slug &&
+//         project.slug !== top2.slug &&
+//         project.slug !== top3.slug,
+//     )
+//     .sort(
+//       (a, b) =>
+//         new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+//         new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
+//     );
 
   return (
     <div className="relative pb-16">
@@ -52,7 +51,7 @@ export default async function ProjectsPage() {
         <div className="w-full h-px bg-zinc-800" />
 
         <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
-          <Card>
+          {/* <Card>
             <Link href={`/projects/${featured.slug}`}>
               <article className="relative w-full h-full p-4 md:p-8">
                 <div className="flex items-center justify-between gap-2">
@@ -91,19 +90,19 @@ export default async function ProjectsPage() {
                 </div>
               </article>
             </Link>
-          </Card>
+          </Card> */}
 
-          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+          {/* <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
             {[top2, top3].map((project) => (
               <Card key={project.slug}>
                 <Article project={project} views={views[project.slug] ?? 0} />
               </Card>
             ))}
           </div>
-        </div>
+        </div> */}
         <div className="hidden w-full h-px md:block bg-zinc-800" />
 
-        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+        {/* <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
           <div className="grid grid-cols-1 gap-4">
             {sorted
               .filter((_, i) => i % 3 === 0)
@@ -131,6 +130,7 @@ export default async function ProjectsPage() {
                 </Card>
               ))}
           </div>
+        </div> */}
         </div>
       </div>
     </div>
